f337f95ae6f7d40805fca990c6ae68abfd129348
Admin <stoicescucatalyn@gmail.com>
2023-02-15 21:34:12 +0200

    implemented add product logic

--- src/main/java/com/sda/OnlineShop/controller/MainController.java
+++ src/main/java/com/sda/OnlineShop/controller/MainController.java
@@ -1,14 +1,34 @@
 package com.sda.OnlineShop.controller;
 
+import com.sda.OnlineShop.dto.ProductDto;
+import com.sda.OnlineShop.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
-@GetMapping("/addProduct")
-    public String addProductGet(){
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/addProduct")
+    public String addProductGet(Model model) {
+        ProductDto productDto = new ProductDto();
+        model.addAttribute("productDto", productDto);
+
         return "addProduct";
     }
 
+    @PostMapping("/addProduct")
+    public String addProductPost(@ModelAttribute ProductDto productDto) {
+        productService.addProduct(productDto);
+        System.out.println(productDto);
 
+        return "addProduct";
+    }
 }
---
+++ src/main/java/com/sda/OnlineShop/entities/Product.java
@@ -0,0 +1,18 @@
+package com.sda.OnlineShop.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+@Entity
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue
+    private Integer productId;
+    private String name;
+    private Integer price;
+    private String category;
+    private Integer quantity;
+    private String description;
+}
